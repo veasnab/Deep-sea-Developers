c2b4541fd8b8a5cbce6f4c0b36879c79a1fbef09
Veasna Bun <97260787+veasnab@users.noreply.github.com>
2023-12-03 20:52:26 -0800

    Delete Phase III/Webpages directory

--- Phase III/Webpages/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'CTD');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING','mysql:dbname=CTD;charset=utf8mb4');
-?>
